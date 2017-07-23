354f2e14464fe9958c0b5c0534a4b346ddc99544
gitusernameplusgithub@gmail.com <gitusernameplusgithub@gmail.com>
2017-07-23 14:52:00 -0400

    playing with argparser - key values off

--- Even_Fibonacci_numbers/main.py
+++ Even_Fibonacci_numbers/main.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 #python 2.7
+import argparse
+
+arguments=argparse.ArgumentParser(description='Fib sequence to what level?.' )
+arguments.add_argument("fibnumber",metavar='N', type=int, nargs='1',help="a fib number")
+
+input= arguments.parse_args()
+print input.accumulate(input.fibnumber)
+
+#import argparse
+
+#parser = argparse.ArgumentParser(description='Process some integers.')
+#parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
+#parser.add_argument('--sum', dest='accumulate', action='store_const',const=sum, default=max, help='sum the integers (default: find the max)')
+
+#args = parser.parse_args()
+#print args.accumulate(args.integers)
 
 #global scope
 fib_number=0
