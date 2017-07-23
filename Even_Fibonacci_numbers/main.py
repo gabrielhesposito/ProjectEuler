10a276260a19226f27e678249ae3dc348202dafe
gitusernameplusgithub@gmail.com <gitusernameplusgithub@gmail.com>
2017-07-23 16:28:12 -0400

    implemented argparse

--- Even_Fibonacci_numbers/main.py
+++ Even_Fibonacci_numbers/main.py
@@ -2,25 +2,19 @@
 #python 2.7
 import argparse
 
-arguments=argparse.ArgumentParser(description='Fib sequence to what level?.' )
-arguments.add_argument("fibnumber",metavar='N', type=int, nargs='1',help="a fib number")
+def get_inputs():
+    arguments=argparse.ArgumentParser(description='Fib sequence to what level?.' )
+    arguments.add_argument("fibnumber",type=int, nargs='?')
+    input= arguments.parse_args()
+    return input.fibnumber
 
-input= arguments.parse_args()
-print input.accumulate(input.fibnumber)
+def calc_fib():
+    fib_number=get_inputs()
+    calculate_fib=0
+    for i in range(0, fib_number):
+        if ( (i % 2) > 0 ):
+	   calculate_fib=calculate_fib+i
+    print(calculate_fib)
 
-#import argparse
-
-#parser = argparse.ArgumentParser(description='Process some integers.')
-#parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
-#parser.add_argument('--sum', dest='accumulate', action='store_const',const=sum, default=max, help='sum the integers (default: find the max)')
-
-#args = parser.parse_args()
-#print args.accumulate(args.integers)
-
-#global scope
-fib_number=0
-for i in range(0, 4000000):
-    if ( (i % 2) > 0 ):
-	fib_number=fib_number+i
-print(fib_number)
+calc_fib()
 
