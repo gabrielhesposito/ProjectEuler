ea7b1d31318ee8535542f61e3c7fc97e0b2f5eeb
gitusernameplusgithub@gmail.com <gitusernameplusgithub@gmail.com>
2017-08-30 18:49:50 -0400

    trying to find prime right now

--- Largest_prime_factor/main.py
+++ Largest_prime_factor/main.py
@@ -6,13 +6,20 @@ def get_inputs ():
     arguments=argparse.ArgumentParser(description='Largest Prime to what level?.')
     arguments.add_argument("--lpf", dest="lpf", required=True, type=int, nargs='?')
     input= arguments.parse_args()
-    return input.fibnumber
+    print input.lpf
+    return input.lpf
 
 
 def calc_lpf():
-    lbf = get_inputs()
-    lbf_stack = array ( lbf  )
-    for i in range(0, lbf ):
-        if ( ( (i+1) % i )  > 0 ):
-           #prime found - ^^ above needs to work
- 
+    lpf = get_inputs()
+    lpf_stack = [ lpf ]
+    for i in range( lpf , 0 , -1 ):
+       if ( ( i % i+1 )  > 0 ):
+         print i
+         continue
+       else:
+         continue
+
+
+
+calc_lpf() 
