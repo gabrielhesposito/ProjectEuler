980e4cc0f0047734d55bcdf81d9427cf8aa4b5e5
gitusernameplusgithub@gmail.com <gitusernameplusgithub@gmail.com>
2017-09-04 18:37:04 -0400

    it finds prime

--- Largest_prime_factor/main.py
+++ Largest_prime_factor/main.py
@@ -15,18 +15,32 @@ def calc_lpf ():
     lpf = get_inputs()
     lpf_stack = []
     topprime=""
-    for i in range( lpf , 1 , -1 ):
-       print "This is the prime evaluated : " +  str(i)
-       for prime in range ( i, 1, -1 ):
-         print "This is the number evaluated : " + str(prime)
-         if ( ( i % prime ) == 0 ) and ( (i / prime) > 1 ) :
-           #not prime
-           time.sleep (2)
-           break
-         else:
-           print "prime found print i" + str(i)
-           lpf_stack.append(i)
-    print lpf_stack
+    for isprime in range( lpf , 1 , -1 ):
+       ########
+       #  
+       #######
+       #print "This is the prime evaluated : " +  str(i)
+       for prime in range ( isprime, 1 , -1 ):         
+         #print "this is isprime: " + str(isprime)
+         #print "this is prime: " + str(prime)
+
+         ##no numbs greater than, no numbs equal too
+         if (prime < isprime) :
+           if (   isprime % prime  == 0  ):
+           #######
+	   #not prime number is divisible
+           #######
+           #time.sleep (2)
+             print "break:  this is isprime: " + str(isprime) + "this is prime: " + str(prime)
+             break
+           
+           print "postbreak: this is isprime: " + str(isprime) + "this is prime: " + str(prime)
+           if (  (prime - 1)  == 1  ):
+             print "prime found print " + str(prime)
+             lpf_stack.append(isprime)
+
+    for element in lpf_stack: 
+         print element
 
 
 calc_lpf() 
